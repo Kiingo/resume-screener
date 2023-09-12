85f64e5b1d77f0855ec3594e3b325f2f02ad26a8
Ross <1750494+RossHartmann@users.noreply.github.com>
2023-09-12 15:53:55 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,85 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['.eslintrc.js', 'jest.config.js'],
+  plugins: [
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  rules: {
+    "curly": "error",
+    "no-self-compare": "error",
+    "no-empty-function": "warn",
+    "no-constant-condition": ["error", { "checkLoops": false }],
+    "no-empty-interface": "warn",
+    // Note: you must disable the base rule as it can report incorrect errors
+    'no-use-before-define': ["warn", { "variables": true, "functions": false, "classes": false }],
+    //"@typescript-eslint/no-use-before-define": ["warn", { "ignoreTypeReferences": true, "typedefs": false, "classes": false }],
+    "no-template-curly-in-string": "warn",
+    "camelcase": "warn",
+    "no-invalid-this": "warn",
+    "no-var": "error",
+    "prefer-const": "warn",
+    "prefer-object-spread": "warn",
+    "prefer-spread": "warn",
+    "arrow-spacing": "error",
+    "prefer-arrow-callback": "error",
+    "no-confusing-arrow": "warn",
+    "consistent-this": ["error", "that"],
+    "eqeqeq": ["warn", "smart"],
+    "no-console": "warn",
+    "prettier/prettier": [
+      "error"
+    ], // Error on prettier issues
+    "semi": [
+      2,
+      "always"
+    ],
+    // We'll make it so that unused-vars error is ignored if we prefix variable with underscore ("_")
+    // https://stackoverflow.com/questions/64052318/how-to-disable-warn-about-some-unused-params-but-keep-typescript-eslint-no-un
+    // We must disable the base rule as it can report incorrect errors if we don't
+    "no-unused-vars": [
+      "error", // or "warn"
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ],
+    "naming-convention": [
+      "error",
+      {
+        "selector": "variable",
+        "types": ["boolean"],
+        "format": ["PascalCase"],
+        "prefix": ["is", "should", "has", "had", "can", "did", "does", "will", "was", "used","use", "were", "are",
+                  "_is", "_should", "_has", "_had", "_can", "_did", "_does", "_will", "_was", "_used", "_use", "_were", "_are"]
+      },
+      {
+        "selector": ["function"],
+        "format": ["camelCase", "PascalCase"],
+        "leadingUnderscore": "allow"
+      },
+      {
+        "selector": "variable",
+        "format": ["camelCase", "PascalCase", "UPPER_CASE"],
+        "leadingUnderscore": "allow"
+      },
+      {
+        "selector": "typeParameter",
+        "format": ["PascalCase"],
+        "prefix": ["T"]
+      },
+      {
+        "selector": "interface",
+        "format": ["PascalCase"],
+        "prefix": ["I"]
+      }
+    ],
+    //"filename-rules/match": [2, /^(([a-z0-9][a-z0-9]*)([-_][a-z0-9]+)*\.?\\?)+$/], // kebab case with dots
+
+  },
+};
