fee55d08e700de1c10e576fcebbcd4f55905ce96
Ross Hartmann <1750494+RossHartmann@users.noreply.github.com>
2024-01-05 22:54:47 -0800

    deploy

--- scripts/git-ops.ts
+++ scripts/git-ops.ts
@@ -187,84 +187,251 @@ const refreshPackages = async ({ forceUpdate }: { forceUpdate: boolean }) => {
       const data = fs.readFileSync(`./${file}`, 'utf8');
       const packageJson = JSON.parse(data);
 
-      // Iterate through dependencies
+      const packages: {
+        isDev: boolean;
+        name: string;
+        version: string;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        dependenciesArray: any;
+      }[] = [];
+
       for (const key of Object.keys(packageJson.dependencies)) {
         if (key.startsWith('@kiingo/')) {
-          let currentVersion = packageJson.dependencies[key];
-          // Remove any leading ^ or ~
-          if (
-            currentVersion.startsWith('^') ||
-            currentVersion.startsWith('~')
-          ) {
-            currentVersion = currentVersion.substring(1);
-          }
-          consola.log(`Found Kiingo package: ${key}`);
-
-          let targetVersion = '';
-          await runCommand(`yarn info ${key}`, {
-            onStdOut: (stdout) => {
-              //consola.success(`${stdout}`);
-              const getLastVersion = (str: string): string | null => {
-                // Regular expression to match the versions array and capture the last version
-                const regex =
-                  /versions:\s*\[\s*(?:'[^']+',\s*)*?'([^']+)'\s*\]/;
-
-                // Execute the regex
-                const matches = regex.exec(str);
-
-                // If a match is found, return the captured group (last version)
-                if (matches && matches[1]) {
-                  return matches[1];
-                }
-
-                // Return null if no match is found
-                return null;
-              };
-
-              // Find the latest version
-              const targetVersionMaybe = getLastVersion(stdout);
-              if (targetVersionMaybe) {
-                // Use this version
-                targetVersion = targetVersionMaybe;
-              } else {
-                throw new Error(`No versions found for package ${key}.`);
-              }
-            },
-            shouldThrowError: false
+          packages.push({
+            isDev: false,
+            name: key,
+            version: packageJson.dependencies[key],
+            dependenciesArray: packageJson.dependencies
           });
+        }
+      }
+      for (const key of Object.keys(packageJson.devDependencies)) {
+        if (key.startsWith('@kiingo/')) {
+          packages.push({
+            isDev: true,
+            name: key,
+            version: packageJson.devDependencies[key],
+            dependenciesArray: packageJson.devDependencies
+          });
+        }
+      }
 
-          if (targetVersion?.length) {
-            if (forceUpdate || targetVersion !== currentVersion) {
-              if (forceUpdate) {
-                consola.log(
-                  `Force updating package: ${key} to ${targetVersion}...`
-                );
-              } else {
-                consola.log(
-                  `${key} is on version ${currentVersion}. Updating to ${targetVersion}...`
-                );
+      // Iterate through dependencies
+      for (const p of packages) {
+        const packageName = p.name;
+        const isDev = p.isDev;
+        let currentVersion = p.version;
+        // Remove any leading ^ or ~
+        if (currentVersion.startsWith('^') || currentVersion.startsWith('~')) {
+          currentVersion = currentVersion.substring(1);
+        }
+        p.dependenciesArray[packageName] = currentVersion;
+        consola.log(`Found Kiingo package: ${packageName}`);
+
+        let targetVersion = '';
+        await runCommand(`yarn info ${packageName}`, {
+          onStdOut: (stdout) => {
+            //consola.success(`${stdout}`);
+            const getLastVersion = (str: string): string | null => {
+              // Regular expression to match the versions array and capture the last version
+              const regex = /versions:\s*\[\s*(?:'[^']+',\s*)*?'([^']+)'\s*\]/;
+
+              // Execute the regex
+              const matches = regex.exec(str);
+
+              // If a match is found, return the captured group (last version)
+              if (matches && matches[1]) {
+                return matches[1];
               }
 
-              consola.log(`Uninstalling package: ${key}`);
-              // Uninstall package using yarn
-              await runCommand(`yarn remove ${key}`, {
-                shouldThrowError: false
-              });
+              // Return null if no match is found
+              return null;
+            };
+            const targetVersionMaybe = getLastVersion(stdout);
+            /*
+            `yarn info ${packageName} versions`
 
-              consola.log(`Re-adding package: ${key}@${targetVersion}`);
-              successMessages.push(
-                `Updated ${key} from ${currentVersion} to ${targetVersion}.`
-              );
-              await runCommand(`yarn add ${key}@${targetVersion}`, {
-                shouldThrowError: false
-              });
+            const getLastVersionFromString = (
+              versionString: string
+            ): string => {
+              // Remove the leading and trailing brackets and split by comma
+              const versions = versionString
+                .slice(1, -1)
+                .split(',')
+                .map((s) => {
+                  // Trim whitespace and strip single/double quotes
+                  return s.trim().replace(/^['"]|['"]$/g, '');
+                });
+
+              if (versions.length === 0) {
+                throw new Error('No versions available');
+              }
+
+              return versions[versions.length - 1];
+            };
+
+            // Find the latest version
+            const targetVersionMaybe = getLastVersionFromString(stdout);
+            */
+
+            if (targetVersionMaybe) {
+              // Use this version
+              targetVersion = targetVersionMaybe;
             } else {
-              consola.success(`${key} is already on version ${targetVersion}.`);
+              throw new Error(`No versions found for package ${packageName}.`);
+            }
+          },
+          shouldThrowError: false
+        });
+
+        if (targetVersion?.length) {
+          if (forceUpdate || targetVersion !== currentVersion) {
+            if (forceUpdate) {
+              consola.log(
+                `Force updating package: ${packageName} to ${targetVersion}...`
+              );
+            } else {
+              consola.log(
+                `${packageName} is on version ${currentVersion}. Updating to ${targetVersion}...`
+              );
+            }
+
+            consola.log(`Uninstalling package: ${packageName}`);
+            // Uninstall package using yarn
+            await runCommand(`yarn remove ${packageName}`, {
+              shouldThrowError: false
+            });
+
+            consola.log(`Re-adding package: ${packageName}@${targetVersion}`);
+            successMessages.push(
+              `Updated ${packageName} from ${currentVersion} to ${targetVersion}.`
+            );
+            await runCommand(
+              `yarn add ${packageName}@${targetVersion}${
+                isDev ? ' --dev' : ''
+              }`,
+              {
+                shouldThrowError: false
+              }
+            );
+
+            // Update the version in package.json cache
+            p.dependenciesArray[packageName] = `${targetVersion}`;
+            p.version = targetVersion;
+
+            // Is this package in peer dependencies? If so, update it there too
+            const peerDependencies = packageJson.peerDependencies;
+            if (peerDependencies && peerDependencies[packageName]) {
+              consola.log(
+                `Updating peer dependency: ${packageName}@${targetVersion}`
+              );
+              peerDependencies[packageName] = targetVersion;
             }
           } else {
-            consola.warn(`No target version found for ${key}.`);
+            consola.success(
+              `${packageName} is already on version ${targetVersion}.`
+            );
+          }
+        } else {
+          consola.warn(`No target version found for ${packageName}.`);
+        }
+      }
+
+      // Now let's detect whether any packages have dependencies that are
+      // different versions (i.e. mismatched versions of the same package).
+
+      // Key is dependency/package name, value is array of versions
+      // If we find multiple versions of the same package, we've got a
+      // version mismatch.
+      const versionDependencies: {
+        [dependencyName: string]: {
+          // Key is The version of the dependency
+          // Value is sourcePackageName (the package that has the dependency with this version)
+          [version: string]: string[];
+        }[];
+      } = {};
+      for (const p of packages) {
+        const packageName = p.name;
+        const version = p.version;
+        await runCommand(`yarn info ${packageName}@${version} dependencies`, {
+          onStdOut: (stdout) => {
+            const parseDependencyString = (
+              dependencyString: string
+            ): Record<string, string> => {
+              const obj: Record<string, string> = {};
+
+              // Remove the outer braces and split by lines
+              const lines = dependencyString.trim().slice(1, -1).split(',');
+
+              for (const line of lines) {
+                // Split by the first colon to separate key and value
+                let [key, value] = line.split(/:(.+)/);
+
+                // Trim and remove extra quotes from key and value
+                key = key.trim().replace(/^['"]|['"]$/g, '');
+                value = value.trim().replace(/^['"]|['"]$/g, '');
+
+                // Remove any leading ^ or ~
+                if (value.startsWith('^') || value.startsWith('~')) {
+                  value = value.substring(1);
+                }
+
+                // We only care about @kiingo packages
+                if (!key.startsWith('@kiingo/')) {
+                  continue;
+                }
+
+                obj[key] = value;
+              }
+
+              return obj;
+            };
+
+            const dependencies = parseDependencyString(stdout) as {
+              [key: string]: string;
+            };
+            for (const dependencyName of Object.keys(dependencies)) {
+              const dependencyVersion = dependencies[dependencyName];
+              if (!versionDependencies[dependencyName]) {
+                versionDependencies[dependencyName] = [];
+              }
+              if (!versionDependencies[dependencyName][dependencyVersion]) {
+                versionDependencies[dependencyName][dependencyVersion] = [];
+              }
+
+              versionDependencies[dependencyName][dependencyVersion].push(
+                packageName
+              );
+            }
+          }
+        });
+      }
+
+      consola.log(`Checking dependency versions...`);
+
+      // Now let's iterate through the dependencies
+      let didFindAnyVersionMismatch = false;
+      for (const dependencyName of Object.keys(versionDependencies)) {
+        const dependencyVersions = versionDependencies[dependencyName];
+
+        if (Object.keys(dependencyVersions).length > 1) {
+          didFindAnyVersionMismatch = true;
+          // We've got a version mismatch
+          consola.error(
+            `Version mismatch detected for dependency ${dependencyName}.`
+          );
+          for (const version of Object.keys(dependencyVersions)) {
+            const sourcePackages = dependencyVersions[version];
+            consola.warn(
+              `Version ${version} is used by the following package(s): ${sourcePackages.join(
+                ', '
+              )}`
+            );
           }
         }
+      }
+      if (!didFindAnyVersionMismatch) {
+        consola.success(`No version mismatches detected!`);
       }
     }
   }
